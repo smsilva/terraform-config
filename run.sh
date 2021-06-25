b9e6f166189e9fc8fe631fe7207ca738dd643089
Silvio Silva <smsilva@gmail.com>
2021-06-25 13:01:57 -0300

    Refactor

--- run.sh
+++ run.sh
@@ -81,4 +81,29 @@ for ENVIRONMENT_DIRECTORY in $(find ./environments/ -type d | sed 1d); do
   release ${STACK_VERSION?} ${ENVIRONMENT?} ${TERRAFORM_CONFIGURATION_FILE?}
 done
 
+cd ${TEMP_INFRA_STACK_LIVE?}
+
+echo "[GIT] Find for changes to commit into: ${TEMP_INFRA_STACK_LIVE?}"
+
+GIT_DIFF_LINE_COUNT=$(git diff | wc -l)
+
+git diff
+
+if [[ ${GIT_DIFF_LINE_COUNT} > 0 ]]; then
+  echo ""
+  echo "  Need to update Live Repo. (lines in diff output: ${GIT_DIFF_LINE_COUNT})"
+
+  git add .
+
+  git commit -m "Build"
+
+  git pull --rebase
+
+  git push
+else
+  echo "  Nothing to do."
+fi
+
+cd - > /dev/null
+
 echo "[END]"
