3a56d4360406bbc913655bc82d0de71d211ed858
Silvio Silva <smsilva@gmail.com>
2021-06-25 11:09:58 -0300

    Refatorando

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+TEMP_DIR="./temp/"
+TEMP_INFRA_STACK_SOURCE_CODE="${TEMP_DIR?}/infra-stack-source-code"
+TEMP_INFRA_STACK_LIVE="${TEMP_DIR?}/infra-stack-live"
+
+mkdir -p ${TEMP_INFRA_STACK_SOURCE_CODE?}
+mkdir -p ${TEMP_INFRA_STACK_LIVE?}
+
+release() {
+  download ${STACK_VERSION?}
+
+  echo "release: ${STACK_VERSION?} ${ENVIRONMENT?} ${TERRAFORM_CONFIGURATION_FILE?}"
+}
 
 download() {
   STACK_VERSION=$1
-
-  TEMP_DIR="./temp/stack"
-  mkdir -p ${TEMP_DIR?}
   
   DOWNLOAD_FILE_NAME_REMOTE="v${STACK_VERSION?}.tar.gz"
-  DOWNLOAD_FILE_NAME_LOCAL="${TEMP_DIR?}/terraform-${DOWNLOAD_FILE_NAME_REMOTE?}"
+  DOWNLOAD_FILE_NAME_LOCAL="${TEMP_INFRA_STACK_SOURCE_CODE?}/terraform-${DOWNLOAD_FILE_NAME_REMOTE?}"
 
   if [ ! -f ${DOWNLOAD_FILE_NAME_LOCAL?} ]; then
     echo ""
@@ -21,11 +30,17 @@ download() {
   
     file ${DOWNLOAD_FILE_NAME_LOCAL?}
 
-    tar xvf ${DOWNLOAD_FILE_NAME_LOCAL?} --directory=${TEMP_DIR?}
+    tar xvf ${DOWNLOAD_FILE_NAME_LOCAL?} --directory=${TEMP_INFRA_STACK_SOURCE_CODE?}
 
     echo ""
   fi
 }
+
+echo "Cloning Stack Infra Live Git Repository"
+
+git clone git@github.com:smsilva/terraform-live.git ${TEMP_INFRA_STACK_LIVE?}
+
+echo ""
 
 for ENVIRONMENT_DIRECTORY in $(find ./environments/ -type d | sed 1d); do
   ENVIRONMENT=$(basename ${ENVIRONMENT_DIRECTORY})
@@ -37,5 +52,5 @@ for ENVIRONMENT_DIRECTORY in $(find ./environments/ -type d | sed 1d); do
   
   echo "stack: ${STACK_VERSION}"
 
-  download ${STACK_VERSION}
+  release ${STACK_VERSION?} ${ENVIRONMENT?} ${TERRAFORM_CONFIGURATION_FILE?}
 done
